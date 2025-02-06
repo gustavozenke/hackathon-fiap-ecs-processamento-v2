3b70e2950d605787c8d59d2cb39cdc37af0d1493
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-06 19:26:36 -0300

    AnonymousCredentialsProvider

--- app/src/main/java/br/com/fiap/hackathon/infraestructure/config/s3/S3Config.java
+++ app/src/main/java/br/com/fiap/hackathon/infraestructure/config/s3/S3Config.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -19,7 +20,7 @@ public class S3Config {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.US_EAST_1)
-//                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(AnonymousCredentialsProvider.create())
                 .build();
     }
 
--- app/src/main/java/br/com/fiap/hackathon/infraestructure/config/sqs/SqsConfig.java
+++ app/src/main/java/br/com/fiap/hackathon/infraestructure/config/sqs/SqsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
@@ -19,7 +20,7 @@ public class SqsConfig {
     public SqsAsyncClient sqsAsyncClient() {
         return SqsAsyncClient.builder()
                 .region(Region.US_EAST_1)
-//                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(AnonymousCredentialsProvider.create())
                 .build();
     }
 
