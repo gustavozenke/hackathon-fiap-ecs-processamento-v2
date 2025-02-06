2e3e3440d967fd8cfc23b9958f6b65bf6d9dbeab
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-06 09:42:14 -0300

    ajustes esteira

--- app/src/main/java/br/com/fiap/hackathon/infraestructure/config/s3/S3Config.java
+++ app/src/main/java/br/com/fiap/hackathon/infraestructure/config/s3/S3Config.java
@@ -3,16 +3,33 @@ package br.com.fiap.hackathon.infraestructure.config.s3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-    @Value("${spring.cloud.aws.region.static}")
-    private String region;
+    @Value("${aws.accessKey}")
+    private String accessKey;
+    @Value("${aws.secretKey}")
+    private String secretKey;
     @Bean
+    @Profile("!local")
     public S3Client s3Client() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
+    }
+
+    @Bean
+    @Profile("local")
+    public S3Client s3ClientLocal() {
         return S3Client.builder()
                 .region(Region.US_EAST_1)
                 .credentialsProvider(DefaultCredentialsProvider.create())
--- app/src/main/java/br/com/fiap/hackathon/infraestructure/config/sqs/SqsConfig.java
+++ app/src/main/java/br/com/fiap/hackathon/infraestructure/config/sqs/SqsConfig.java
@@ -3,16 +3,32 @@ package br.com.fiap.hackathon.infraestructure.config.sqs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Configuration
 public class SqsConfig {
-    @Value("${spring.cloud.aws.region.static}")
-    private String region;
+    @Value("${aws.accessKey}")
+    private String accessKey;
+    @Value("${aws.secretKey}")
+    private String secretKey;
     @Bean
+    @Profile("!local")
     public SqsAsyncClient sqsAsyncClient() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return SqsAsyncClient.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    @Profile("local")
+    public SqsAsyncClient sqsAsyncClientLocal() {
         return SqsAsyncClient.builder()
                 .region(Region.US_EAST_1)
                 .credentialsProvider(DefaultCredentialsProvider.create())
